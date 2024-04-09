95c323547f5b297c9e0d69ffa5c0042be2367218
Sara Valle <svj971@gmail.com>
2024-04-09 17:02:06 -0500

    feat: add tab buttons and styles

---
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -0,0 +1,10 @@
+import "./TabButton.css";
+function TabButton({ children, onClick }) {
+	return (
+		<li>
+			<button onClick={onClick}>{children}</button>
+		</li>
+	);
+}
+
+export default TabButton;
