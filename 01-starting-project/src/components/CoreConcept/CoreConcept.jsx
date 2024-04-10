992d1529b127c0c64249d3c3f76a94907a2a37a5
Sara Valle <svj971@gmail.com>
2024-04-09 19:48:04 -0500

    fix: add key for li items

--- 01-starting-project/src/components/CoreConcept/CoreConcept.jsx
+++ 01-starting-project/src/components/CoreConcept/CoreConcept.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./CoreConcept.css";
 
 function CoreConcept(props) {
-	const { keyIndex, image, title, description } = props;
+	const { image, title, description } = props;
 
 	return (
-		<li key={keyIndex}>
+		<li>
 			<img src={image} alt={title} />
 			<h3>{title}</h3>
 			<p>{description}</p>
