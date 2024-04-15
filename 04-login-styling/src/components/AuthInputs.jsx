904216254043d02b2171837158e8d316b2d75a2f
Sara Valle <svj971@gmail.com>
2024-04-15 16:58:17 -0500

    feat: add tailwind styles

--- 04-login-styling/src/components/AuthInputs.jsx
+++ 04-login-styling/src/components/AuthInputs.jsx
@@ -1,15 +1,7 @@
 import { useState } from "react";
 
-import styled from "styled-components";
 import Button from "./Button";
 import Input from "./Input";
-
-const ControlComponent = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 0.5rem;
-	margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
 	const [enteredEmail, setEnteredEmail] = useState("");
@@ -32,8 +24,11 @@ export default function AuthInputs() {
 	const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
 	return (
-		<div id="auth-inputs">
-			<ControlComponent>
+		<div
+			id="auth-inputs"
+			className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+		>
+			<div className="flex flex-col gap-2 mb-6">
 				<Input
 					labelText="Email"
 					type="Email"
@@ -49,9 +44,9 @@ export default function AuthInputs() {
 						handleInputChange("password", event.target.value)
 					}
 				/>
-			</ControlComponent>
-			<div className="actions">
-				<button type="button" className="text-button">
+			</div>
+			<div className="flex justify-end gap-4">
+				<button type="button" className="text-amber-400 hover:text-amber-500">
 					Create a new account
 				</button>
 				<Button onClick={handleLogin}>Sign In</Button>
