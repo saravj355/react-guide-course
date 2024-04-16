209a84b413e87564a5c0f726909a4570ec840beb
Sara Valle <svj971@gmail.com>
2024-04-16 15:30:57 -0500

    add refs and portals

--- 06-refs-and-portals/src/components/Player.jsx
+++ 06-refs-and-portals/src/components/Player.jsx
@@ -1,11 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const playerName = useRef();
+
+	const [name, setName] = useState("");
+
+	function handleClick() {
+		setName(playerName.current.value);
+		playerName.current.value = "";
+	}
+
+	return (
+		<section id="player">
+			<h2>Welcome {name || "Unknown entity"}</h2>
+			<p>
+				<input type="text" ref={playerName} />
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
